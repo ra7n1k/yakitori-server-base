c340129af55a58cb7fd73ccdd59243c8567ef1c4
ra7n1k <civilization.destruction@gmail.com>
2024-01-08 17:15:24 +0900

    Updated to distribute items to players logging in for the first time.

--- src/main/java/tk/yakitori/yakitoriserverbase/FirstJoin.java
+++ src/main/java/tk/yakitori/yakitoriserverbase/FirstJoin.java
@@ -1,2 +1,48 @@
-package tk.yakitori.yakitoriserverbase;public class FirstJoin {
+package tk.yakitori.yakitoriserverbase;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+
+import static org.bukkit.ChatColor.*;
+
+public class FirstJoin implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+            player.sendMessage(ChatColor.BOLD + "Welcome to " + ChatColor.ITALIC + "Yakitori Server" + ChatColor.RESET + ChatColor.BOLD + "!");
+
+            TextComponent termsLink = new TextComponent("terms of use");
+            termsLink.setColor(BLUE.asBungee());
+            termsLink.setUnderlined(true);
+            ClickEvent termsLinkClick = new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.yakitori.tk/terms");
+            termsLink.setClickEvent(termsLinkClick);
+
+
+            TextComponent terms = new TextComponent("Please read the ");
+            terms.addExtra(termsLink);
+            terms.addExtra(" (available in Japanese and English).");
+
+            player.spigot().sendMessage(terms);
+
+        if (!player.hasPlayedBefore()) {
+            ItemStack[] items = {
+                    new ItemStack(Material.STONE_PICKAXE),
+                    new ItemStack(Material.STONE_AXE),
+                    new ItemStack(Material.STONE_SHOVEL),
+                    new ItemStack(Material.STONE_SWORD),
+                    new ItemStack(Material.BREAD, 32)
+            };
+            for (int i = 0; i < items.length; i++) {
+                player.getInventory().setItem(i, items[i]);
+            }
+        }
+    }
 }
--- src/main/java/tk/yakitori/yakitoriserverbase/YakitoriServerBase.java
+++ src/main/java/tk/yakitori/yakitoriserverbase/YakitoriServerBase.java
@@ -1,6 +1,12 @@
 package tk.yakitori.yakitoriserverbase;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import static org.bukkit.Bukkit.getPluginManager;
 
 public final class YakitoriServerBase extends JavaPlugin {
 
@@ -8,6 +14,7 @@ public final class YakitoriServerBase extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+        getServer().getPluginManager().registerEvents(new FirstJoin(), this);
     }
 
     @Override
