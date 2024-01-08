b1f1e925df35bbac126036919dee33f1ff274ab7
ra7n1k <civilization.destruction@gmail.com>
2024-01-08 10:59:56 +0900

    Initial commit

---
+++ src/main/java/tk/yakitori/yakitoriserverbase/FirstJoin.java
@@ -0,0 +1,2 @@
+package tk.yakitori.yakitoriserverbase;public class FirstJoin {
+}
---
+++ src/main/java/tk/yakitori/yakitoriserverbase/YakitoriServerBase.java
@@ -0,0 +1,17 @@
+package tk.yakitori.yakitoriserverbase;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class YakitoriServerBase extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
